56e0ec4d82215b215e9344a94738f5b0803a64bf
July <106986983+July-vilh@users.noreply.github.com>
2025-02-22 21:17:09 +0200

    minor update

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -11,7 +11,7 @@ let consonantsQuan = 0;
 
 let vowelsList = 'aeiouAEIOU';
 let consonantsList = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ';
-let specialChars = ' !@#$%^&*()_+[]{}|;:,.<>?/~`=\\\'"';
+let specialChars = "- !@#$%^&*()_+[]{}|;:,.<>?/~`=\\'-'\"";
 
 for (let i = 0; i < word.length; i++) {
   if (specialChars.includes(word[i])) {
