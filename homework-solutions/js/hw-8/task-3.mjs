4dd65ed61b54506f844686c0a915a52b8268eac9
July <106986983+July-vilh@users.noreply.github.com>
2025-03-04 20:44:50 +0200

    hw8

--- homework-solutions/js/hw-8/task-3.mjs
+++ homework-solutions/js/hw-8/task-3.mjs
@@ -5,9 +5,17 @@
 
   Пример: const arr = [5,2,7,3,8,1,6] //4
 */
+'use strict';
 
 function findMissingNumber(numbers) {
-  // Ваш код
+  let lengthExpected = numbers.length + 1;
+  let expectedSum = (lengthExpected * (lengthExpected + 1)) / 2;
+  let realSum = numbers.reduce((result, value) => result + value, 0);
+  let emptyValue = expectedSum - realSum;
+  return emptyValue;
 }
+
+let numbers = [5, 2, 7, 3, 8, 1, 6];
+console.log(findMissingNumber(numbers));
 
 export { findMissingNumber };
